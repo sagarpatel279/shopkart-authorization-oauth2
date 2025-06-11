2e2084502a05bbd1d835cb94732029edec2860b9
sagarpatel279 <patelsagar913@gmail.com>
2025-06-11 10:12:37 +0530

    init the project

---
+++ src/main/java/com/shopkart/authorization/oauth2/ShopkartAuthorizationOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.shopkart.authorization.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartAuthorizationOauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopkartAuthorizationOauth2ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopkart/authorization/oauth2/ShopkartAuthorizationOauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopkart.authorization.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartAuthorizationOauth2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
